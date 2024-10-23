7313961aae817e8fab0fff79fd47f98eb1ee3953
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-10-23 16:38:35 +0500

    flatlist with fetch api

---
+++ flatlist_with_fetchapi/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <header className="mx-auto  bg-gray-500 items-center justify-between p-6 lg:px-8">
+    <div className=''>
+        <h1>Footer</h1>
+    </div>
+    </header>
+  )
+}
+
+export default Footer
---
+++ flatlist_with_fetchapi/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="mx-auto  bg-gray-500 items-center justify-between p-6 lg:px-8">
+    <div className='text-center text-2xl font-semibold'>
+        <h1>Header</h1>
+    </div>
+    </header>
+  )
+}
+
+export default Header
