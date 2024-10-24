e0499dc50cb9a484b306f89e88b7da69dd865795
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2024-10-24 21:22:52 +0500

    Update App.js

--- crud/src/App.js
+++ crud/src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
       <h1 className="text-center font-bold text-2xl text-gray-700">
-        CRUD with redux toolkit
+    // this is heading
+        CRUD with redux toolkit    
       </h1>
       <Routes>
         <Route path="/" element={<UserList />} />
